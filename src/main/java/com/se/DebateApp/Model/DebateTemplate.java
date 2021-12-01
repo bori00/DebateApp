ceb37f846408ac4198e7318e94f036776595d3ce
bori00 <fazakasbori@gmail.com>
2021-12-01 20:48:43 +0200

    add missing fields to the debate templates

--- src/main/java/com/se/DebateApp/Model/DebateTemplate.java
+++ src/main/java/com/se/DebateApp/Model/DebateTemplate.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "debate_templates")
@@ -21,10 +22,26 @@ public class DebateTemplate {
     private User owner;
 
     @Column(nullable = true, length = 100)
+    private String title;
+
+    @Column(nullable = true, length = 100)
     private String topic;
 
     @Column(nullable = false, length = 1000)
     private String statement;
 
+    @OneToMany
+    private Set<LinkToResource> resourceLinks;
 
+    @Column(nullable = false)
+    private Integer prepTimeSeconds;
+
+    @Column(nullable = false)
+    private Integer constSpeechSeconds;
+
+    @Column(nullable = false)
+    private Integer rebuttalSpeechSeconds;
+
+    @Column(nullable = false)
+    private Integer crossExaminationSeconds;
 }
