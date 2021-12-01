fe2a4041c0b57693ac9edf2a62312f7c18d9ba90
bori00 <fazakasbori@gmail.com>
2021-12-01 11:30:40 +0200

    initial commit

---
+++ src/test/java/com/se/DebateApp/DebateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.DebateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
