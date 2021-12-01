2d572e28075105bcf8a795109a68300cdf48b7c7
bori00 <fazakasbori@gmail.com>
2021-12-01 18:55:03 +0200

    handle unsuccessful registrations

--- src/main/java/com/se/DebateApp/Controller/RegistrationController.java
+++ src/main/java/com/se/DebateApp/Controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.se.DebateApp.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,23 @@ public class RegistrationController {
     private UserRepository userRepository;
 
     @PostMapping("/process_register")
-    public String processRegistration(User user) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        userRepository.save(user);
-        return "register_success";
+    public String processRegistration(User user, Model model) {
+        if (!userNameIsTaken(user.getUserName())) {
+            // Valid new user data.
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String encodedPassword = encoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+            userRepository.save(user);
+            return "register_success";
+        } else {
+            // invalid username: username is taken.
+            model.addAttribute("errorMessage", "This username is already taken. Please choose " +
+                    "another one!");
+            return "register";
+        }
+    }
+
+    private boolean userNameIsTaken(String userName) {
+        return userRepository.findByUserName(userName) != null;
     }
 }
