1d73dadf90954a216eb228348f6a6959122dbd0d
bori00 <fazakasbori@gmail.com>
2021-12-01 14:51:19 +0200

    handle successful registrations

--- src/main/java/com/se/DebateApp/Controller/LoginRegisterController.java
+++ src/main/java/com/se/DebateApp/Controller/LoginRegisterController.java
@@ -1,12 +1,18 @@
 package com.se.DebateApp.Controller;
 
 import com.se.DebateApp.Model.User;
+import com.se.DebateApp.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginRegisterController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("")
     public String viewHomePage() {
@@ -17,5 +23,11 @@ public class LoginRegisterController {
     public String showSignUpForm(Model model) {
         model.addAttribute("user", new User());
         return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+        userRepository.save(user);
+        return "register_success";
     }
 }
