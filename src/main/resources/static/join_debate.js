d0b103d6cb6ed475c9248e9c07b051fb26d98250
keresztesbeata <keresztesbeata00@yahoo.com>
2021-12-07 17:19:34 +0200

    join existing  meeting with url

---
+++ src/main/resources/static/join_debate.js
@@ -0,0 +1,71 @@
+/*
+ * Main functionalities:
+ * - setting up the call frame for the meeting room
+ * - joining a call (room)
+ */
+
+let callFrame;
+
+async function createCallFrame() {
+    const callWrapper = document.getElementById('wrapper');
+    callFrame = await window.DailyIframe.createFrame(callWrapper, {
+        iframeStyle: {
+            height: '100%',
+            width: '100%',
+            aspectRatio: 16 / 9,
+            minWidth: '500px',
+            maxWidth: '920px',
+            border: '0',
+            borderRadius: '12px',
+        },
+    });
+
+    callFrame.on('joined-meeting', handleJoinedMeeting)
+        .on('left-meeting', handleLeftMeeting);
+
+    const callURL = document.getElementById('url-input');
+    const joinButton = document.getElementById('join-call');
+    callURL.addEventListener('input', () => {
+        joinButton.toggleAttribute('disabled');
+        if (callURL.checkValidity()) {
+            joinButton.toggleAttribute('disabled');
+        }
+    });
+}
+
+async function joinDebate() {
+    const url = document.getElementById('url-input').value;
+
+    try {
+        await callFrame.join({
+            url: url,
+            showLeaveButton: true,
+            showParticipantsBar: true,
+            showFullScreenButton: true,
+        });
+    } catch (e) {
+        toggleUrlInputPrompt();
+        console.error(e);
+    }
+}
+
+/* Event listener callbacks and helpers */
+
+/**
+ * Hide the url input form after joining the meeting, and show it again after leaving the call.
+ */
+function toggleUrlInputPrompt() {
+    const inputForm = document.getElementById('url-input-form');
+
+    inputForm.classList.toggle('hide');
+}
+
+function handleJoinedMeeting() {
+    toggleUrlInputPrompt();
+}
+
+function handleLeftMeeting() {
+    toggleUrlInputPrompt();
+    const callURL = document.getElementById('url-input');
+    callURL.setAttribute('placeholder','Enter room URL...')
+}
