93ce0e3b0828fe96130446a898b21f76d750490d
bori00 <fazakasbori@gmail.com>
2021-12-11 02:07:00 +0200

    allow starting debate as a judge only if the judge has no other ongoing debates

--- src/main/java/com/se/DebateApp/Controller/StartDebateController.java
+++ src/main/java/com/se/DebateApp/Controller/StartDebateController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -31,8 +32,17 @@ public class StartDebateController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final String errorMessageName = "errorMessage";
+    private static final String hasOtherOngoingDebateErrorMsg = "You can't start a new debate, " +
+            "when you " +
+            "are a player/judge in an other ongoing debate. Please retry later!";
+
     @GetMapping("/process_start_debate")
     public String processStartDebateSession(@RequestParam Long debateTemplateId, Model model) {
+        if (hasOngoingDebate(getCurrentUser())) {
+            model.addAttribute(errorMessageName, hasOtherOngoingDebateErrorMsg);
+            return "start_debate";
+        }
         Optional<DebateTemplate> optDebateTemplate =
                 debateTemplateRepository.findById(debateTemplateId);
         if (optDebateTemplate.isEmpty()) {
@@ -46,5 +56,21 @@ public class StartDebateController {
         DebateSession savedSession = debateSessionRepository.save(debateSession);
         model.addAttribute("debateCode", savedSession.getId());
         return "start_debate";
+    }
+
+    boolean hasOngoingDebate(User user) {
+        List<DebateSession> ongoingSessionsAsJudge =
+                debateSessionRepository.findActiveDebateSessionsOfJudge(user);
+        if (!ongoingSessionsAsJudge.isEmpty()) {
+            return true;
+        }
+        List<DebateSession> ongoingDebatesAsPlayer =
+                debateSessionRepository.findActiveDebateSessionsOfPlayer(user);
+        return !ongoingDebatesAsPlayer.isEmpty();
+    }
+
+    private User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByUserName(((CustomUserDetails) auth.getPrincipal()).getUsername());
     }
 }
--- src/main/java/com/se/DebateApp/Repository/DebateSessionRepository.java
+++ src/main/java/com/se/DebateApp/Repository/DebateSessionRepository.java
@@ -2,7 +2,18 @@ package com.se.DebateApp.Repository;
 
 import com.se.DebateApp.Model.DebateSession;
 import com.se.DebateApp.Model.DebateTemplate;
+import com.se.DebateApp.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DebateSessionRepository extends JpaRepository<DebateSession, Long> {
+    @Query("Select s FROM DebateSession s WHERE s.debateTemplate.owner = ?1 and s" +
+            ".debateSessionPhase <> '-'")
+    List<DebateSession> findActiveDebateSessionsOfJudge(User user);
+
+    @Query("Select s FROM DebateSession s, IN(s.players) p WHERE p.user = ?1 and s" +
+            ".debateSessionPhase <> '-'")
+    List<DebateSession> findActiveDebateSessionsOfPlayer(User user);
 }
