1c33e7367d0098f61c6a37a6b079db7b3f7ca1bd
bori00 <fazakasbori@gmail.com>
2021-12-16 00:56:02 +0200

    create DTO for response to a join debate request:

--- src/main/java/com/se/DebateApp/Controller/StartDebate/DTOs/JoinDebateRequestResponse.java
+++ src/main/java/com/se/DebateApp/Controller/StartDebate/DTOs/JoinDebateRequestResponse.java
@@ -1,14 +1,10 @@
-package com.se.DebateApp.Controller.StartDebate;
+package com.se.DebateApp.Controller.StartDebate.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
 public class JoinDebateRequestResponse {
 
     public static final String HAS_OTHER_ONGOING_DEBATE_ERROR_MSG = "You can't join a new debate," +
@@ -16,12 +12,12 @@ public class JoinDebateRequestResponse {
             "when you are a player/judge in an other ongoing debate. Please retry later!";
 
     public static final String DEBATE_NOT_FOUND_ERROR_MSG = "Seems like the " +
-            "requested debate does not exist. Pleasy verify that you entered the correct code.";
+            "requested debate does not exist. Please verify that you entered the correct code.";
 
-    public static final String DEBATE_NOLONGER_ACCEPTING_PLAYERS_ERROR_MSG = "This debate no " +
+    public static final String DEBATE_NO_LONGER_ACCEPTING_PLAYERS_ERROR_MSG = "This debate no " +
             "longer accepts players. We're sorry, but you cannot join the debate.";
 
-    private boolean success;
-    private String errorMessage;
+    private final boolean success;
+    private final String errorMessage;
 
 }
