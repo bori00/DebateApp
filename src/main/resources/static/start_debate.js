d0f4393bdc9ffead83daea97f43da9fb8f771f51
bori00 <fazakasbori@gmail.com>
2021-12-16 03:41:14 +0200

    display the participants' status on the judge's page

--- src/main/resources/static/start_debate.js
+++ src/main/resources/static/start_debate.js
@@ -14,8 +14,22 @@ async function subscribeToParticipantAnnouncementSocket() {
     console.log("Socket initialized");
 
     stompClient.connect({}, function (frame) {
-        stompClient.subscribe("/user/queue/debate-session-participants-status",  function (message) {
-            console.log("New message from the server: " + message);
-        });
+        stompClient.subscribe("/user/queue/debate-session-participants-status",
+            function (participantsStatusResponse) {
+                const participantsStatus = JSON.parse(participantsStatusResponse.body);
+                console.log("New participants status: " + participantsStatus);
+                updateUIWithNewParticipantsStatus(participantsStatus);
+            });
     });
+}
+
+function updateUIWithNewParticipantsStatus(participantsStatus) {
+    $('#noWaitingToJoinParticipantsTd')[0].innerText=participantsStatus.noWaitingToJoinParticipants;
+    $('#noProParticipantsTd')[0].innerText=participantsStatus.noProParticipants;
+    $('#noConParticipantsTd')[0].innerText=participantsStatus.noConParticipants;
+    $('#activate-debate-session-button')[0].disabled = participantsStatus.canActivateSession;
+}
+
+function activateDebateSession() {
+    console.log("Activate...");
 }
