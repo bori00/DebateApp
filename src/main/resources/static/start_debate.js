2be2ac2e08cf9f88c8d5d774e294443e38148d1c
bori00 <fazakasbori@gmail.com>
2021-12-16 19:15:06 +0200

    solve bug with removeAttrbute on activbate debate button

--- src/main/resources/static/start_debate.js
+++ src/main/resources/static/start_debate.js
@@ -28,7 +28,7 @@ function updateUIWithNewParticipantsStatus(participantsStatus) {
     $('#noProParticipantsTd')[0].innerText=participantsStatus.noProParticipants;
     $('#noConParticipantsTd')[0].innerText=participantsStatus.noConParticipants;
     if (participantsStatus.canActivateSession) {
-        $('#activate-debate-session-button')[0].removeAttr('disabled');
+        $('#activate-debate-session-button')[0].removeAttribute('disabled');
     } else {
         $('#activate-debate-session-button')[0].setAttribute('disabled', '');
     }
