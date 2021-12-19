0ac3374906122c2d77be90bebbc1160ac71cc909
bori00 <fazakasbori@gmail.com>
2021-12-19 14:45:37 +0200

    remove and regenerate API key

--- src/main/resources/static/local.config.js
+++ src/main/resources/static/local.config.js
@@ -1,5 +1,5 @@
 // dev API key related to the account
-const DAILY_API_KEY = "7a4340e30a0dd579b1212597c7435180e4e4664a4a06895c32f420d2dcc9c8f2"
+const DAILY_API_KEY = ""
 
 // the application domain, which is used for REST operations: get/create/update rooms
 const DAILY_REST_DOMAIN = "https://api.daily.co/v1"
