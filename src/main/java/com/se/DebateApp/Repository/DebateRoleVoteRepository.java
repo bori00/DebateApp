72466d7211ecc589eb5aca5d579d7453fdf4264c
bori00 <fazakasbori@gmail.com>
2021-12-22 21:49:54 +0200

    add debate role vote repository

---
+++ src/main/java/com/se/DebateApp/Repository/DebateRoleVoteRepository.java
@@ -0,0 +1,7 @@
+package com.se.DebateApp.Repository;
+
+import com.se.DebateApp.Model.DebateRoleVote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DebateRoleVoteRepository extends JpaRepository<DebateRoleVote, Long> {
+}
