30930d362318a16080010a345ba119792894f354
bori00 <fazakasbori@gmail.com>
2021-12-22 21:12:43 +0200

    define DTOs for deputy candidates

---
+++ src/main/java/com/se/DebateApp/Controller/DeputySelection/DTOs/DeputyCandidatesListDTO.java
@@ -0,0 +1,19 @@
+package com.se.DebateApp.Controller.DeputySelection.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DeputyCandidatesListDTO {
+    private final List<DeputyCandidateDTO> deputyCandidates;
+    private final boolean success;
+    private final String errorMsg;
+
+    public static final String UNEXPECTED_ERROR_MSG = "An unexpected error occurred. Please retry" +
+            " later.";
+    public static final String PHASE_PASSED_MSG = "Seems like you missed the voting phase. We're " +
+            "sorry.";
+}
