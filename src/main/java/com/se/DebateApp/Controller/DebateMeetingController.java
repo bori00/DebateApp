9cf02badf01c8cd87a9d113e50c46afe6925ff21
bori00 <fazakasbori@gmail.com>
2021-12-22 23:31:44 +0200

    refactor controllers

--- src/main/java/com/se/DebateApp/Controller/DebateMeetingController.java
+++ src/main/java/com/se/DebateApp/Controller/DebateMeetingController.java
@@ -36,13 +36,7 @@ public class DebateMeetingController {
     private DebateMeetingRepository debateMeetingRepository;
 
     @Autowired
-    private DebateSessionPlayerRepository debateSessionPlayerRepository;
-
-    @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
 
     @PostMapping(value = "/process_create_meeting", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -66,113 +60,6 @@ public class DebateMeetingController {
         return debateMeetings.stream()
                 .map(debateMeeting -> new DebateMeetingDTO(debateMeeting.getName(), debateMeeting.getUrl(), debateMeeting.getMeetingType().getCode()))
                 .collect(Collectors.toList());
-    }
-
-    @GetMapping(value = "/process_get_username_of_current_user")
-    @ResponseBody
-    public String processGetUserNameOfCurrentUser() {
-        return getCurrentUser().getUserName();
-    }
-
-    @GetMapping(value = "/process_get_debate_session_player", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public DebateSessionPlayerDTO processGetDebateSessionPlayer(@RequestParam(value = "debateSessionId") Long debateSessionId) {
-        DebateSessionPlayer debateSessionPlayer = debateSessionPlayerRepository.findDebateSessionPlayerByUserAndDebateSession(getCurrentUser(), debateSessionRepository.getById(debateSessionId))
-                .orElseThrow(() -> new NotFoundException("The given user is not a player in the debate session"));
-
-        DebateSessionPlayerDTO debateSessionPlayerDTO = new DebateSessionPlayerDTO();
-
-        debateSessionPlayerDTO.setDebateSessionId(debateSessionPlayer.getDebateSession().getId());
-        debateSessionPlayerDTO.setId(debateSessionPlayer.getId());
-        debateSessionPlayerDTO.setPlayerRole(debateSessionPlayer.getPlayerRole().getCode());
-        debateSessionPlayerDTO.setPlayerState(debateSessionPlayer.getPlayerState().getCode());
-        debateSessionPlayerDTO.setTeam(debateSessionPlayer.getTeam().getCode());
-
-        return debateSessionPlayerDTO;
-    }
-
-    @GetMapping("/process_get_time_interval")
-    @ResponseBody
-    public Integer processGetTimeInterval(@RequestParam(value = "debateSessionId") Long debateSessionId) {
-        DebateSession debateSession = debateSessionRepository.getById(debateSessionId);
-        DebateTemplate debateTemplate = debateSession.getDebateTemplate();
-
-        switch (debateSession.getDebateSessionPhase()) {
-            case PREP_TIME -> {
-                return debateTemplate.getPrepTimeSeconds();
-            }
-            case AFFIRMATIVE_CONSTRUCTIVE_SPEECH_1, AFFIRMATIVE_CONSTRUCTIVE_SPEECH_2, NEGATIVE_CONSTRUCTIVE_SPEECH_1, NEGATIVE_CONSTRUCTIVE_SPEECH_2 -> {
-                return debateTemplate.getConstSpeechSeconds();
-            }
-            case CROSS_EXAMINATION_1, CROSS_EXAMINATION_2, CROSS_EXAMINATION_3, CROSS_EXAMINATION_4 -> {
-                return debateTemplate.getCrossExaminationSeconds();
-            }
-            case AFFIRMATIVE_REBUTTAL_1, AFFIRMATIVE_REBUTTAL_2, NEGATIVE_REBUTTAL_1, NEGATIVE_REBUTTAL_2 -> {
-                return debateTemplate.getRebuttalSpeechSeconds();
-            }
-            default -> {
-                return 24 * 60 * 60; // 24 hours
-            }
-        }
-    }
-
-    @GetMapping(value = "/process_get_current_phase_starting_time", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public Date processGetCurrentPhaseStartingTime(@RequestParam(value = "debateSessionId") Long debateSessionId) {
-        DebateSession debateSession = debateSessionRepository.getById(debateSessionId);
-
-        return debateSession.getCurrentPhaseStartingTime();
-    }
-
-    @GetMapping(value = "/process_is_debate_finished")
-    @ResponseBody
-    public Boolean processIsDebateFinished(@RequestParam(value = "debateSessionId") Long debateSessionId) {
-        DebateSession debateSession = debateSessionRepository.getById(debateSessionId);
-
-        return debateSession.getDebateSessionPhase().equals(FINISHED);
-    }
-
-    @PostMapping("/process_end_of_current_phase")
-    @ResponseBody
-    public void processEndOfCurrentPhase(@RequestParam Long debateSessionId) {
-        DebateSession debateSession = debateSessionRepository.getById(debateSessionId);
-        DebateSessionPhase currentPhase = debateSession.getDebateSessionPhase();
-        if(currentPhase.equals(FINISHED)) {
-            return;
-        }
-        int nextPhase = currentPhase.ordinal() + 1;
-        debateSession.setDebateSessionPhase(DebateSessionPhase.values()[nextPhase]);
-        debateSession.setCurrentPhaseStartingTime(new Date(System.currentTimeMillis()));
-        debateSessionRepository.save(debateSession);
-        announceAllDebatePlayersAboutEndOfTimeInterval(debateSession.getPlayers(), currentPhase);
-    }
-
-    @GetMapping("/process_close_debate")
-    public String processCloseDebate() {
-        List<DebateSession> ongoingDebateSessions = debateSessionRepository.findDebateSessionsOfJudgeWithStateDifferentFrom(getCurrentUser(), FINISHED);
-        if(ongoingDebateSessions.size() != 1) {
-            return "error";
-        }
-        DebateSession debateSession = ongoingDebateSessions.get(0);
-        debateSession.setDebateSessionPhase(FINISHED);
-        debateSessionRepository.save(debateSession);
-        announceAllDebatePlayersAboutDebateClosed(debateSession.getPlayers());
-        return "home";
-    }
-
-    private void announceAllDebatePlayersAboutDebateClosed(Set<DebateSessionPlayer> players) {
-        for (DebateSessionPlayer player : players) {
-            simpMessagingTemplate.convertAndSendToUser(
-                    player.getUser().getUserName(), "queue/debate-closed", "closed");
-        }
-    }
-
-    private void announceAllDebatePlayersAboutEndOfTimeInterval(Set<DebateSessionPlayer> players, DebateSessionPhase phase) {
-        String destinationUrl = "/queue/debate-" + phase.name().toLowerCase().replace('_', '-') + "-times-up";
-        for (DebateSessionPlayer player : players) {
-            simpMessagingTemplate.convertAndSendToUser(
-                    player.getUser().getUserName(), destinationUrl, "timesUp");
-        }
     }
 
     private DebateMeeting createDebateMeeting(DebateMeetingAttributes debateMeetingAttributes) {
